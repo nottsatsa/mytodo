0deb176131db9459a6c153560a48f25d31fb5b5b
Tsatsral Sainbolor <tsatsralsainbolor@Tsatsrals-MacBook-Air.local>
2025-09-23 19:09:04 +0800

    cute font imported

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,6 +5,6 @@ export const Input = ({ task_name, input_value }) => {
       value={input_value}
       onChange={task_name}
       className={` h-[40px] w-[280px] bg-white rounded-[6px] border-[1px] border-solid border-black px-[12px] py-[4px] flex text-black`}
-      placeholder={"addtask"}></input>
+      placeholder={"Add task here..."}></input>
   );
 };
